14e3595728555fb2a0971638d3cae8aff05e8948
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-03-06 16:14:24 -0500

    Adding percentage bars

--- src/newPages/PercentBar.js
+++ src/newPages/PercentBar.js
@@ -3,16 +3,46 @@ import { Row, Col } from 'reactstrap';
 import { NumberWidget } from 'components/Widget';
 import { dataJS } from 'newComponents/dataJS';
 
-const theArr = dataJS.data.map(key => {
-  return key.total_vaccinated;
-});
-
-const percentPopVac = () => {
-  const temp = theArr[theArr.length - 1];
-  return (temp / 30000000) * 100;
-};
-
 const PercentBar = () => {
+  const dateArr = dataJS.data.map(key => {
+    return key.date;
+  });
+
+  const theArr = dataJS.data.map(key => {
+    return key.total_vaccinated;
+  });
+
+  const theArr2 = dataJS.data.map(key => {
+    return key.total_cases;
+  });
+
+  const theArr3 = dataJS.data.map(key => {
+    return key.total_fatalities;
+  });
+
+  const theArr4 = dataJS.data.map(key => {
+    return key.total_recoveries;
+  });
+
+  const percentPopVac = () => {
+    const temp = theArr[theArr.length - 1];
+    return parseFloat(((temp / 37742154) * 100).toFixed(3));
+  };
+
+  const percentPopInf = () => {
+    const temp2 = theArr2[theArr2.length - 1];
+    return parseFloat(((temp2 / 37742154) * 100).toFixed(3));
+  };
+
+  const percentPopFat = () => {
+    const temp2 = theArr3[theArr2.length - 1];
+    return parseFloat(((temp2 / 37742154) * 100).toFixed(4));
+  };
+
+  const percentInfRec = () => {
+    const temp2 = theArr4[theArr4.length - 1];
+    return parseFloat(((temp2 / 37742154) * 100).toFixed(4));
+  };
   console.log(theArr);
   return (
     <Row>
@@ -24,19 +54,43 @@ const PercentBar = () => {
           color="secondary"
           progress={{
             value: percentPopVac(),
-            label: 'As of today',
+            label: 'As of ' + dateArr[dateArr.length - 1],
           }}
         />
       </Col>
       <Col lg={3} md={6} sm={6} xs={12}>
         <NumberWidget
-          title="Total Profit"
-          subtitle="This month"
-          number="9.8k"
+          title="Total cumulative infected"
+          subtitle="Percentage of pop infected"
+          number={theArr2[theArr.length - 1]}
           color="secondary"
           progress={{
-            value: 75,
-            label: 'Last month',
+            value: percentPopInf(),
+            label: 'As of ' + dateArr[dateArr.length - 1],
+          }}
+        />
+      </Col>
+      <Col lg={3} md={6} sm={6} xs={12}>
+        <NumberWidget
+          title="Total Covid Fatalities"
+          subtitle="Percentage of pop fatalities"
+          number={theArr3[theArr.length - 1]}
+          color="secondary"
+          progress={{
+            value: percentPopFat(),
+            label: 'As of ' + dateArr[dateArr.length - 1],
+          }}
+        />
+      </Col>
+      <Col lg={3} md={6} sm={6} xs={12}>
+        <NumberWidget
+          title="Total Recoveries"
+          subtitle="Percentage of population recovered"
+          number={theArr4[theArr.length - 1]}
+          color="secondary"
+          progress={{
+            value: percentInfRec(),
+            label: 'As of ' + dateArr[dateArr.length - 1],
           }}
         />
       </Col>
