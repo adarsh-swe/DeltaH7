5260d93bfc5fb6a1bd29263fb0985473607e5d13
DhrumilP <pandyd3@mcmaster.ca>
2021-03-06 16:38:54 -0500

    Hooked up thumbnail and title from NewsCard to News

--- src/newPages/NewsCard.js
+++ src/newPages/NewsCard.js
@@ -8,17 +8,12 @@ import {
   CardTitle,
 } from 'reactstrap';
 
-const NewsCard = (thumbnail, title, description, url) => {
+const NewsCard = ({ thumbnail, title, description, url }) => {
   return (
     <Card>
-      <CardImg
-        top
-        width="100%"
-        src="/assets/318x180.svg"
-        alt="Card image cap"
-      />
+      <CardImg top width="100%" src={thumbnail} alt="Card image cap" />
       <CardBody>
-        <CardTitle tag="h5">Card title</CardTitle>
+        <CardTitle tag="h5">{title}</CardTitle>
         <CardText>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
