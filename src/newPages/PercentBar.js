c18d020ad566807ce427b9a670e1292341f11129
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-03-06 15:32:44 -0500

    Adding stuff

--- src/newPages/PercentBar.js
+++ src/newPages/PercentBar.js
@@ -7,18 +7,24 @@ const theArr = dataJS.data.map(key => {
   return key.total_vaccinated;
 });
 
+const percentPopVac = () => {
+  const temp = theArr[theArr.length - 1];
+  return (temp / 30000000) * 100;
+};
+
 const PercentBar = () => {
+  console.log(theArr);
   return (
     <Row>
       <Col lg={3} md={6} sm={6} xs={12}>
         <NumberWidget
-          title="Percentage of population Vaccinated"
-          subtitle="This month"
-          number="9.8k"
+          title="Population Vaccinated"
+          number={theArr[theArr.length - 1]}
+          subtitle="Percentage of pop vaccinated"
           color="secondary"
           progress={{
-            value: 75,
-            label: 'Last month',
+            value: percentPopVac(),
+            label: 'As of today',
           }}
         />
       </Col>
