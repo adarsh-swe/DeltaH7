45592c6d2ef3152ba1d70f8bab54a7f858df4e82
DhrumilP <pandyd3@mcmaster.ca>
2021-03-06 16:34:45 -0500

    Config file for news

---
+++ src/newPages/news-config.js
@@ -0,0 +1,97 @@
+export const news = [
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/HBJu4uMigVaO96TBt6ysjg--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDtzbT0xO3c9MTY0/https://media.zenfs.com/en/cbc.ca/6ad7bca1253cb87174c6cbaffae3f270',
+    title: 'Ontario sees 990 new COVID-19 cases and 6 deaths',
+    description:
+      'Ontario is reporting 990 new cases of COVID-19 and six new deaths, according to the latest provincial figures. The new daily case count brings the total... ',
+    url: 'https://ca.news.yahoo.com/ontario-sees-990-covid-19-152645240.html',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/hCCzNXjXVKR9b48zDjWxMQ--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDtzbT0xO3c9MTY0/https://media.zenfs.com/en/cbc.ca/6b6d22aabef80d8e4cfc6b4159bb36fc',
+    title:
+      'Ontario sees 966 new COVID-19 cases as questions over vaccine timing linger',
+    description:
+      'Ontario reported another 966 cases of COVID-19 on Tuesday, as debate continues around whether the... ',
+    url: 'https://ca.news.yahoo.com/ontario-sees-966-covid-19-152929074.html',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/lqUpKSvaTcaUmnNqUuzdAA--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDt3PTE2NA--/https://s.yimg.com/am/60d/a9f43cd113af21e833987be5ef920e2b',
+    title:
+      'COVID-19: Ontario reports spike in variants and 1,062 new cases as total case count surpasses...',
+    description:
+      'Ontario’s total case count passed another grim milestone. Ontario is reporting 1,062 new... ',
+    url:
+      'https://ottawacitizen.com/news/covid-19-ontario-reports-spike-in-variants-and-1062-new-cases-as-total-case-count-surpasses-300000',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/N6hpBV1CoMDLDvhPF9.z1Q--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDt3PTE2NA--/https://s.yimg.com/am/60d/d58b64af53a3b89357d0bdba693ca409',
+    title: 'Ontario records fewer than 1,000 new COVID-19 cases',
+    description:
+      'Ontario is reporting a single-day drop in the number of new COVID-19 cases with less than 1,000 new infections logged. Health officials recorded 990 new... ',
+    url:
+      'https://toronto.ctvnews.ca/ontario-records-fewer-than-1-000-new-covid-19-cases-1.5336598',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/ZaudZvPgPTsSyCAMS099Ag--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDtzbT0xO3c9MTY0/https://media.zenfs.com/en/cbc.ca/78abcb625ff166241597148554662261',
+    title:
+      'Why Toronto&#39;s COVID-19 vaccination rollout is slower than other Ontario cities',
+    description:
+      'The rollout of COVID-19 vaccinations in Ontario is happening at 34 different speeds, with each... ',
+    url:
+      'https://ca.news.yahoo.com/why-torontos-covid-19-vaccination-100000798.html',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/8hPjN5y6GhoY.qHrIProNQ--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDt3PTE2NA--/https://s.yimg.com/am/60d/4a4fd4af7cb41209a7ea2cb8949e4691',
+    title:
+      'In face of deadly pandemic, Ontario long-term care homes continue breaking COVID-19 safety rules |...',
+    description:
+      '&quot;To have egregious infractions in terms of not following standard operating procedure for things... ',
+    url:
+      'https://www.cbc.ca/news/health/covid-long-term-care-ontario-violations-1.5933549',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/ehlkfXyE0Vlfg6uw3CyMmg--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDt3PTE2NA--/https://s.yimg.com/am/60d/722da2094a45ec609e84a85ca46ae2d0',
+    title:
+      'COVID-19: Ontario sees 966 new cases; 10 staff, clients test positive at Ottawa physical distancing...',
+    description:
+      'New COVID-19 cases in Ontario dropped below 1,000 on Monday, with the province reporting 966 new... ',
+    url:
+      'https://ottawacitizen.com/news/local-news/covid-19-ontario-sees-966-new-cases-10-staff-clients-test-positive-at-ottawa-physical-distancing-centres',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/BHy1I6T0GVjb8C1m2MM5yQ--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDtzbT0xO3c9MTY0/https://media.zenfs.com/en/cbc.ca/380f4210669e6c5d658c6162bbe5db0a',
+    title:
+      'COVID-linked deaths in Ontario top 7,000 as record-high number get vaccine shots',
+    description:
+      'Ontario reported another 958 cases of COVID-19 on Wednesday, as the number of deaths linked to the... ',
+    url:
+      'https://ca.news.yahoo.com/covid-linked-deaths-ontario-top-152751523.html',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/M_PHcuBq8gUtB.o2udXflQ--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDt3PTE2NA--/https://s.yimg.com/am/60d/b6b7bd97e903c81029f7dfa803c68e66',
+    title:
+      'Ontario to release updated COVID-19 vaccine rollout plan today | CBC News',
+    description:
+      'Ontario officials are expected to provide an update on the province&#39;s COVID-19 vaccine rollout on... ',
+    url:
+      'https://www.cbc.ca/news/canada/toronto/covid-19-ontario-march-5-2021-toronto-peel-vaccines-1.5937850',
+  },
+  {
+    thumbnail:
+      'https://s.yimg.com/fz/api/res/1.2/qOpG4aGQrsu1PKI7rEjPVg--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD05MjtweG9mZj0wO3B5b2ZmPTA7cT04MDtzbT0xO3c9MTY0/https://media.zenfs.com/en/cbc_testing_241/c222a4643e1d7bcc50c0eb9a2a8d61c2',
+    title:
+      'Ontario reports 1,062 new COVID-19 cases and 20 deaths as province surpasses 300,000 infections',
+    description:
+      'Christine Elliott said the new cases include 259 in Toronto, 201 in Peel and 86 in York. Meanwhile,... ',
+    url: 'https://ca.news.yahoo.com/ontario-reports-1-062-covid-152524965.html',
+  },
+];
